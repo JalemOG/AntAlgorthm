aa2a89dbeb4d8349870c6df2585ea1a9cf9ac269
RamOL2682 <o.ramirez.1@estudiantec.cr>
2024-11-02 23:41:08 -0600

    centrado de la página root

--- main.py
+++ main.py
@@ -3,5 +3,14 @@ from simulator import AntSimulationApp
 
 if __name__ == "__main__":
     root = tk.Tk()
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    x = (screen_width - 600) // 2
+    y = (screen_height - 850) // 2
+
+    root.geometry(f"{600}x{850}+{x}+{y}")
+    root.update()
+
     app = AntSimulationApp(root)
     root.mainloop()
